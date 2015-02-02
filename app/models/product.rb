3895b2ea30424729d7328fc6be50ad48e83eb5c0
Lewis Coulson <lewis.coulson@wearefriday.com>
2015-02-02 14:54:42 +0000

    Iteration D2: Connecting Products to Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,22 @@ class Product < ActiveRecord::Base
 	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
+  private
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
