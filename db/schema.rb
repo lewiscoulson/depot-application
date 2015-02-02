b5edccc9b1f6cd0fad9ecbbfd4a7e1f1b54ef39f
Lewis Coulson <lewis.coulson@wearefriday.com>
2015-02-02 17:14:17 +0000

    Iteration E1: Creating a Smarter Cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202153422) do
+ActiveRecord::Schema.define(version: 20150202170433) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
