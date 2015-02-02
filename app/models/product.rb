86a3d8ea2a5ffb21852c48519396803ab43eb394
Lewis Coulson <info@lewiscoulson.com>
2015-02-02 07:57:47 +0000

    Iteration D1: Finding a Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ActiveRecord::Base
 	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+	def self.latest 
+		Product.order(:updated_at).last
+	end
 end
