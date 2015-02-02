08b9e2464e535b1801b6898675dc7747955225e0
Lewis Coulson <lewis.coulson@wearefriday.com>
2015-02-02 14:47:19 +0000

    Iteration D1: Finding a Cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127073413) do
+ActiveRecord::Schema.define(version: 20150202144440) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
