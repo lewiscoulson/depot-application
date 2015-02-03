587da9081dbb9418b1370a14f2823d9c45335b07
Lewis Coulson <info@lewiscoulson.com>
2015-02-03 08:09:49 +0000

    Iteration E2: Handling Errors

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150202170433) do
+
   create_table "carts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
