5cc1a0434fec0805bcf4e9a121ffb62e9a1d6eaf
Lewis Coulson <lewis.coulson@wearefriday.com>
2015-02-04 17:19:37 +0000

    Capturing the Order Details

---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
+
+  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+end
